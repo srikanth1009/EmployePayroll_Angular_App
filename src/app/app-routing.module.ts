165a9ffd1f22d64d7acd9fe39d88797cc078d539
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2022-01-07 16:27:44 +0530

    Added: Created dashboard  part for empleepayroll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HeaderComponent} from "./component/header/header.component"
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'empheader', component:HeaderComponent},
+{
+  path: 'dashbord' , component: DashBoardComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
