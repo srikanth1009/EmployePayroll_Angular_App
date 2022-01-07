0b5e473dd3fee2aaf99167ce89a26ce94bcc5555
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2022-01-07 16:36:25 +0530

    Added: Created dashboard  part for empleepayroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
